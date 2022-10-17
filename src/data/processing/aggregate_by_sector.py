771026edfacc553c5369bd6ca699fd34300ac9d4
YPeter Van Katwyk <pvankatwyk@gmail.com>
2022-10-17 15:37:11 -0400

    ran aggregate + readability

--- src/data/processing/aggregate_by_sector.py
+++ src/data/processing/aggregate_by_sector.py
@@ -2,6 +2,7 @@ from data.classes.AtmosphereForcing import AtmosphereForcing
 from data.classes.GridSectors import GridSectors
 from utils import get_all_filepaths
 import pandas as pd
+import numpy as np
 import time
 
 def aggregate_by_sector(path):
@@ -29,7 +30,8 @@ def aggregate_by_sector(path):
 
 def aggregate_all(af_directory):
     """Loops through every NC file in the provided atmospheric forcing directory
-    and 
+    from 1995-2100 and applies the aggregate_by_sector function. It then outputs
+    the concatenation of all processed data to all_data.csv 
 
     Args:
         af_directory (str): Directory containing atmospheric forcing files
@@ -38,6 +40,9 @@ def aggregate_all(af_directory):
     filepaths = get_all_filepaths(path=af_directory, filetype='nc')
     filepaths = [f for f in filepaths if "1995-2100" in f]
 
+    print(f"Files to be processed...")
+    print([f.split("/")[-1] for f in filepaths])
+
     all_data = pd.DataFrame()
     for i, fp in enumerate(filepaths):
         print('')
@@ -45,6 +50,13 @@ def aggregate_all(af_directory):
         print(f'File: {fp.split("/")[-1]}')
         print(f'Time since start: {(time.time()-start_time) // 60} minutes')
         atmosphere = aggregate_by_sector(fp)
+
+        # Handle files that don't have mrro_anomaly input
+        try:
+            atmosphere.data['mrro_anomaly']
+        except KeyError:
+            atmosphere.data['mrro_anomaly'] = np.nan
+
         atmosphere.data = atmosphere.data[['pr_anomaly', 'evspsbl_anomaly', 'mrro_anomaly', 'smb_anomaly', 'ts_anomaly', 'regions', 'model',]]
         atmosphere.data.to_csv(f"{fp[:-3]}_sectoryeargrouped.csv")
         all_data = pd.concat([all_data, atmosphere.data])
