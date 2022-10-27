5981421f06bb37271908e0f34c4e29593cfd4bac
Peter Van Katwyk <pvankatwyk@gmail.com>
2022-10-27 10:10:21 -0400

    delete old files

--- startup.sh
+++
@@ -1,6 +0,0 @@
-# screen -S interact
-# screen -r interact
-# interact -n 4 -m 12g -t 01:00:00
-# cd /users/pvankatw/data/pvankatw/pvankatw-bfoxkemp/GHub-ISMIP6-Forcing/AIS/Atmosphere_Forcing/
-git pull
-export PYTHONPATH="${PYTHONPATH}:/users/pvankatw/emulator"
