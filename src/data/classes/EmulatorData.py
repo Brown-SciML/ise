aeb281e0018f1480f38157c2d0e58e5b0c31afec
Peter Van Katwyk <pvankatwyk@gmail.com>
2022-11-02 13:10:35 -0400

    working model, process method, better training progress

--- src/data/classes/EmulatorData.py
+++ src/data/classes/EmulatorData.py
@@ -9,7 +9,6 @@ class EmulatorData:
         
         self.directory = directory
         
-        self.output_columns = ['icearea', 'iareafl', 'iareagr', 'ivol', 'ivaf', 'smb', 'smbgr', 'bmbfl']
         
         try:
             self.data = pd.read_csv(f"{self.directory}/master.csv", low_memory=False)
@@ -19,20 +18,61 @@ class EmulatorData:
                 self.outputs = pd.read_csv(f"{self.directory}/outputs.csv")
             except FileNotFoundError:
                 raise FileNotFoundError('Files not found, make sure to run all processing functions.')
-            
+        
+        self.output_columns = ['icearea','iareafl','iareagr','ivol','ivaf','smb','smbgr', 'bmbfl']
         self.X = None
         self.y = None
         self.scaler_X = None
         self.scaler_y = None
+        
+    def process(self, target_column, drop_missing=True, drop_columns=True, boolean_indices=True, scale=True, ):
+        if drop_missing:
+            self = self.drop_missing()
+        if drop_columns:
+            if drop_columns is True:
+                self = self.drop_columns(columns=['experiment', 'exp_id', 'groupname', 'regions'])
+            elif isinstance(drop_columns, list):
+                self = self.drop_columns(columns=drop_columns)
+            else:
+                raise ValueError(f'drop_columns argument must be of type boolean|list, received {type(drop_columns)}')
+        
+        if boolean_indices:
+            self = self.create_boolean_indices()
+        
+        self = self.split_data(target_column=target_column)
+        
+        if scale:
+            self.X = self.scale(self.X, 'inputs', scaler='MinMaxScaler')
+            self.y = self.scale(self.y, 'outputs', scaler='MinMaxScaler')
+        
+        self = self.train_test_split()
+        
+        return self, self.train_features, self.test_features, self.train_labels, self.test_labels
+            
     
     def split_data(self, target_column: str, ):
         self.target_column = target_column
         self.X = self.data.drop(columns=self.output_columns)
         self.y = self.data[target_column]
+        self.input_columns = self.X.columns
         return self
     
     def train_test_split(self, train_size=0.7, shuffle=True):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, train_size=train_size, random_state=42, shuffle=False)
+        # if isinstance(self.X, pd.DataFrame):
+        #     train_dataset = dataset.sample(frac=0.8, random_state=0)
+        #     test_dataset = dataset.drop(train_dataset.index)
+        # self.train_features, self.test_features, self.train_labels, self.test_labels = train_test_split(self.X, self.y, train_size=train_size, random_state=0, shuffle=False)
+        
+        if not isinstance(self.X, pd.DataFrame):
+            self.X = pd.DataFrame(self.X, columns=self.input_columns)
+    
+        self.train_features = self.X.sample(frac=train_size, random_state=0)
+        training_indices = self.train_features.index
+        self.train_labels = self.y[training_indices].squeeze()
+        
+        self.test_features = self.X.drop(training_indices)
+        self.test_labels = pd.Series(self.y.squeeze()).drop(training_indices)
+        
         return self
         
     def drop_missing(self):
@@ -47,6 +87,9 @@ class EmulatorData:
                 raise ValueError(f'Columns argument must be of type: list, received {type(columns)}.')
             
             self.data = pd.get_dummies(self.data, columns=columns)
+            
+            for col in self.data.columns:
+                self.data[col] = self.data[col].astype(float)
         return self
     
     def drop_columns(self, columns):
@@ -55,6 +98,7 @@ class EmulatorData:
         columns = list(columns)
         
         self.data = self.data.drop(columns=columns)
+            
         return self
     
     def scale(self, values, values_type, scaler="MinMaxScaler"):
@@ -74,7 +118,7 @@ class EmulatorData:
         if 'input' in values_type.lower():
             self.input_columns = self.X.columns
             self.scaler_X.fit(self.X)
-            return self.scaler_X.transform(values)
+            return pd.DataFrame(self.scaler_X.transform(values), columns=self.X.columns)
         
         elif 'output' in values_type.lower():
             self.scaler_y.fit(np.array(self.y).reshape(-1,1))
--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
 cfg = get_configs()
 import pandas as pd
+import time
 
 forcing_directory = cfg['data']['forcing']
 zenodo_directory = cfg['data']['output']
@@ -47,30 +48,28 @@ if processing['combine_datasets']:
 
 # TODO: Start using my EmulatorData class -- find out what step is causing problems
 emulator_data = EmulatorData(directory=export_dir)
-# data = pd.read_csv(r"/users/pvankatw/emulator/src/data/files/master.csv", low_memory=False)
-data = emulator_data.drop_missing().drop_columns(columns=['experiment', 'exp_id', 'groupname', 'regions'])
-# dataset = data.dropna().drop(columns=['experiment', 'exp_id', 'groupname', 'regions'])
-dataset = pd.get_dummies(data.data)
+# emulator_data = emulator_data.drop_missing().drop_columns(columns=['experiment', 'exp_id', 'groupname', 'regions']).create_boolean_indices()
+# emulator_data = emulator_data.split_data(target_column='ivaf')
+# emulator_data.X = emulator_data.scale(emulator_data.X, 'inputs', scaler='MinMaxScaler')
+# emulator_data.y = emulator_data.scale(emulator_data.y, 'outputs', scaler='MinMaxScaler')
+# emulator_data = emulator_data.train_test_split()
 
-for col in dataset.columns:
-    dataset[col] = dataset[col].astype(float)
-    
-scaler = MinMaxScaler()
-scaler.fit(dataset)
-dataset = pd.DataFrame(scaler.transform(dataset), columns=dataset.columns)
+emulator_data, train_features, test_features, train_labels, test_labels = emulator_data.process(
+    target_column='ivaf',
+    drop_missing=True,
+    drop_columns=['experiment', 'exp_id', 'groupname', 'regions'],
+    boolean_indices=True,
+    scale=True,
+)
 
-train_dataset = dataset.sample(frac=0.8, random_state=0)
-test_dataset = dataset.drop(train_dataset.index)
+# train_features = emulator_data.train_features
+# test_features = emulator_data.test_features
+# train_labels = emulator_data.train_labels
+# test_labels = emulator_data.test_labels
 
-output_cols = ['icearea','iareafl','iareagr','ivol','ivaf','smb','smbgr', 'bmbfl']
-train_features = train_dataset.drop(columns=output_cols)
-test_features = test_dataset.drop(columns=output_cols)
-train_labels = train_dataset['ivaf']
-test_labels = test_dataset['ivaf']
-
-class Model(torch.nn.Module):
+class Emulator(torch.nn.Module):
     def __init__(self):
-        super(Model, self).__init__()
+        super(Emulator, self).__init__()
         self.input_layer_size = X_train.shape[1]
         self.layer = torch.nn.Linear(self.input_layer_size, 128)
         self.layer2 = torch.nn.Linear(128, 32)
@@ -83,7 +82,7 @@ class Model(torch.nn.Module):
         x = self.layer3(x)     
         return x
 
-class EmulatorDataset(Dataset):
+class EmulatorTrainingDataset(Dataset):
     
     def __init__(self, X_data, y_data):
         self.X_data = X_data
@@ -95,29 +94,26 @@ class EmulatorDataset(Dataset):
     def __len__ (self):
         return len(self.X_data)
     
+    
 X_train = np.array(train_features, dtype=np.float64)
 y_train = np.array(train_labels, dtype=np.float64)
 X_test = np.array(test_features, dtype=np.float64)
 y_test = np.array(test_labels, dtype=np.float64)
-train_dataset = EmulatorDataset(torch.from_numpy(X_train).float(), torch.from_numpy(y_train).float())
-test_dataset = EmulatorDataset(torch.from_numpy(X_test).float(), torch.from_numpy(y_test).float())
 
+train_dataset = EmulatorTrainingDataset(torch.from_numpy(X_train).float(), torch.from_numpy(y_train).float().squeeze())
 train_loader = DataLoader(dataset=train_dataset, batch_size=100)
-test_loader = DataLoader(dataset=test_dataset, batch_size=1)
 
-model = Model()
+model = Emulator()
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(),)
 epochs = 100
 verbose = True
 loss_list = []
-for epoch in range(epochs):
-    if verbose and epoch % 1 == 0:
-        print(f'---------- EPOCH: {epoch} ----------')
+for epoch in range(1, epochs+1):
 
     iteration = 0
+    total_loss = 0
     for X_train_batch, y_train_batch in train_loader:
-        
         # Train model
         optimizer.zero_grad()
 
@@ -125,130 +121,28 @@ for epoch in range(epochs):
         loss = criterion(pred, y_train_batch.unsqueeze(1))
         loss.backward()
         optimizer.step()
-    loss_list.append(loss)
+        
+        total_loss += loss.item()
+            
+    # loss_list.append(loss)
+    avg_loss = total_loss / len(train_loader)
+    loss_list.append(avg_loss)
 
-    if iteration % 20 == 0:
-        print(f"MSE: {loss}")
+    if epoch % 5 == 0:
+        print(f"Epoch: {epoch}, MSE: {avg_loss}")
         
 model.eval()
 X_test = torch.tensor(X_test, dtype=torch.float)
 preds = model(X_test)
 
-print(f"""--- Random Forest ---
+print('')
+print(f"""--- PyTorch Test Metrics---
 Mean Absolute Error: {mean_absolute_error(y_test, preds.detach().numpy())}
 Mean Squared Error: {mean_squared_error(y_test, preds.detach().numpy())}
 R2 Score: {r2_score(y_test, preds.detach().numpy())}""")
 
 plt.scatter(preds.detach().numpy(), y_test, s=3)
 plt.plot([min(y_test),max(y_test)], [min(y_test),max(y_test)], 'r-')
-plt.savefig("nn.png")
-
-
-
-
-
-# data = EmulatorData(directory=data_directory)
-# data = data.drop_missing().drop_columns(columns=['exp_id', 'groupname', 'experiment'])
-# data = data.create_boolean_indices()
-
-# for col in data.data.columns:
-#     data.data[col] = data.data[col].astype(float)
-    
-# data = data.split_data(target_column='ivol')
-# data.X = data.scale(values=data.X, values_type='inputs', scaler="MinMaxScaler")
-# data.y = data.scale(values=data.y, values_type='outputs', scaler="MinMaxScaler")
-# data = data.train_test_split(shuffle=True)
-
-# # rf = RandomForestRegressor(verbose=2, n_jobs=-1)
-# # rf.fit(data.X_train, data.y_train)
-# # preds = rf.predict(data.X_test)
-
-# # print(f"""--- Random Forest ---
-# # Mean Absolute Error: {mean_absolute_error(data.y_test, preds)}
-# # Mean Squared Error: {mean_squared_error(data.y_test, preds)}
-# # R2 Score: {r2_score(data.y_test, preds)}""")
-
-# # plt.plot(data.y_test, preds, 'o')
-# # plt.plot([0,1],[0,1],'-')
-# # plt.savefig('rf.png')
-
-# class Net(torch.nn.Module):
-#    def __init__(self):
-#        super(Net, self).__init__()
-#        self.input_layer_size = data.X_train.shape[1]
-#        self.layer = torch.nn.Linear(self.input_layer_size, 128)
-#        self.layer2 = torch.nn.Linear(128, 32)
-#        self.layer3 = torch.nn.Linear(32, 1)
-#        self.relu = nn.ReLU()
-
-#    def forward(self, x):
-#        x = self.relu(self.layer(x))
-#        x = self.relu(self.layer2(x))
-#        x = self.layer3(x)     
-#        return x
-   
-# class EmulatorDataset(Dataset):
-    
-#     def __init__(self, X_data, y_data):
-#         self.X_data = X_data
-#         self.y_data = y_data
-        
-#     def __getitem__(self, index):
-#         return self.X_data[index], self.y_data[index]
-        
-#     def __len__ (self):
-#         return len(self.X_data)
-
-# X_train = np.array(data.X_train, dtype=np.float64)
-# y_train = np.array(data.y_train, dtype=np.float64)
-# X_test = np.array(data.X_test, dtype=np.float64)
-# y_test = np.array(data.y_test, dtype=np.float64)
-# train_dataset = EmulatorDataset(torch.from_numpy(X_train).float(), torch.from_numpy(y_train).float())
-# test_dataset = EmulatorDataset(torch.from_numpy(X_test).float(), torch.from_numpy(y_test).float())
-
-# train_loader = DataLoader(dataset=train_dataset, batch_size=1000, shuffle=True)
-# test_loader = DataLoader(dataset=test_dataset, batch_size=1)
-
-# model = Net()
-# criterion = nn.MSELoss()
-# optimizer = optim.Adam(model.parameters(),)
-# epochs = 100
-# verbose = True
-# for epoch in range(epochs):
-#     if verbose and epoch % 1 == 0:
-#         print(f'---------- EPOCH: {epoch} ----------')
-
-#     iteration = 0
-#     for X_train_batch, y_train_batch in train_loader:
-        
-#         # Train model
-#         optimizer.zero_grad()
-
-#         pred = model(X_train_batch)
-#         loss = criterion(pred, y_train_batch.unsqueeze(1))
-#         loss.backward()
-#         optimizer.step()
-
-#     if iteration % 20 == 0:
-#         print(f"MSE: {loss}")
-        
-# y_pred_list = []
-# with torch.no_grad():
-#     model.eval()
-#     for X_batch, _ in test_loader:
-#         y_test_pred = model(X_batch)
-#         y_pred_list.append(y_test_pred.numpy())
-# y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
-
-# # preds = preds.numpy().squeeze()
-    
-# print(f"""--- Random Forest ---
-# Mean Absolute Error: {mean_absolute_error(y_test, y_pred_list)}
-# Mean Squared Error: {mean_squared_error(y_test, y_pred_list)}
-# R2 Score: {r2_score(y_test, y_pred_list)}""")
-
-# plt.plot(y_test, y_pred_list, 'o')
-# plt.plot([0,1],[0,1],'-')
-# plt.savefig('nn.png')
+# plt.savefig("nn.png")
 
 stop = ''
