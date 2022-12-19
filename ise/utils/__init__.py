61aa8448ce4e4ae669df37f52846b625ab242c4c
Peter Van Katwyk <pvankatwyk@gmail.com>
2022-12-19 11:22:41 -0500

    latest pdoc html export

--- ise/utils/__init__.py
+++ ise/utils/__init__.py
@@ -1,6 +1,6 @@
 from ise.utils.utils import (
     get_all_filepaths, check_input, 
-    plot_true_vs_predicted, _structure_architecture_args, _structure_emulatordata_args
+    _structure_architecture_args, _structure_emulatordata_args
 )
 from ise.utils.data import (
     load_ml_data, undummify, combine_testing_results, get_uncertainty_bands
