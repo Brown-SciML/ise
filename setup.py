4ea4b626ad360585ae0c307bf988603526f48433
Peter Van Katwyk <pvankatwyk@gmail.com>
2022-12-21 09:45:16 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ise',
+   version='1.0.0',
+   description='Ice Sheet Emulator Library',
+   author='Peter Van Katwyk',
+   author_email='pvnakatwyk@gmail.com',
+   packages=['ise'],  #same as name
+   install_requires=['torch'], #external packages as dependencies
+)
