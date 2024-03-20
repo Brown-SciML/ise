e46ecfff26beba721157b347f3295e87a40c18ac
Peter Van Katwyk <pvankatwyk@gmail.com>
2024-03-20 15:13:21 -0400

    fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ise",
-    version="0.0.1",
+    version="2.0.0",
     description="Package for creating ice sheet emulators predicting future sea level rise.",
     author="Peter Van Katwyk",
     author_email="pvankatwyk@gmail.com",
